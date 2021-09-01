22c7f00fa7259a47eab93ee89abfb611f39c0737
UMA SHARMA <umasharma316@gmail.com>
2021-09-01 18:02:17 +0530

    added grid to contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,28 +7,27 @@
                     <div class="w-full mb-4">
                         <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
                     </div>
-            <div class="grid grid-cols-3 gap-4">
+            <div class="grid grid-cols-3 divide-x sm:divide-y-1 justify-center">
               <div class="text-gray-700 text-2xl mb-8">
                   <p>Address</p><br>
-                            <p class="text-lg">
-                                ERTS Lab,<br>
-                                First Floor, KReSIT Building,<br>
-                                IIT Bombay, Powai,<br>
-                                Mumbai - 400076, Maharashtra.<br>
-                                022 2576 4986<br>
-                                022 25720184
-                            </p> 
+                    <p class="text-lg">
+                        ERTS Lab,<br>
+                        First Floor, KReSIT Building,<br>
+                        IIT Bombay, Powai,<br>
+                        Mumbai - 400076, Maharashtra.<br>
+                        022 2576 4986<br>
+                        022 25720184
+                    </p> 
               </div>
-              <!-- ... -->
               <div class="text-gray-700 text-2xl mb-8">
                   <p>Email Addresses for our initiatives</p><br>
-                            <p class="text-lg">
-                                eYRC: eyrc@e-yantra.org<br>
-                                eYIC: eyic@e-yantra.org<br>
-                                eYSRC: school@e-yantra.org<br>
-                                eLSI: support@e-yantra.org<br>
-                                eFSI: efsi@e-yantra.org
-                            </p>  
+                    <p class="text-lg">
+                        eYRC: eyrc@e-yantra.org<br>
+                        eYIC: eyic@e-yantra.org<br>
+                        eYSRC: school@e-yantra.org<br>
+                        eLSI: support@e-yantra.org<br>
+                        eFSI: efsi@e-yantra.org
+                    </p>  
               </div>
               <div class="text-gray-700 text-2xl mb-8 justify-center m-6">
                   <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15077.924545593301!2d72.9159513!3d19.1304053!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xfa4dcc5535605048!2sERTS%20Lab!5e0!3m2!1sen!2sin!4v1630045629215!5m2!1sen!2sin" width="300" height="150" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
