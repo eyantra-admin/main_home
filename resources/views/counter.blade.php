6ebe0e72251b03889f6ae6a17dc69f0143b65db2
UMA SHARMA <umasharma316@gmail.com>
2021-09-09 14:06:12 +0530

    changed count

--- resources/views/counter.blade.php
+++ resources/views/counter.blade.php
@@ -236,7 +236,7 @@ footer p strong {
             </div>
             <div class="item wow fadeInUpBig animated animated" data-number="55" style="visibility: visible;">
                 <i class="fa fa-smile-o"></i>
-                <p id="number2" class="number">&nbsp; 3200 +</p>
+                <p id="number2" class="number">&nbsp; 3245 +</p>
                 <span></span>
                 <p>Colleges Benefited</p>
             </div>
