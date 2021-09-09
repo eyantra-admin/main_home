624b8e15a5da511c592d0044c4c807bfd75fce70
UMA SHARMA <umasharma316@gmail.com>
2021-09-09 14:04:27 +0530

    added portal url

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
         <div class="container-fluid mx-auto flex flex-col md:flex-row items-center bg-gray-200 md:pt-24 justify-center shade">
             <!--Left Col-->
             <div class="flex flex-col w-full justify-center items-start py-18 lg:ml-24 sm:ml-6 text-gray-700 text-xl pb-6">
-                <span class="inline-flex items-center justify-center px-2 py-1 text-xl font-bold leading-none text-red-100 bg-gray-600 rounded-full mt-28 ml-4">e-Yantra Robotics Competitions Launched !!!<a href="portal.e-yantra.org" target="_blank"><span class="inline-flex items-center justify-center px-2 py-1 text-xl font-bold leading-none text-red-100 bg-red-600 rounded-full">Go</span></a></span><br>
+                <span class="inline-flex items-center justify-center px-2 py-1 text-xl font-bold leading-none text-red-100 bg-gray-600 rounded-full mt-28 ml-4">e-Yantra Robotics Competitions Launched !!!<a href="https://portal.e-yantra.org/" target="_blank"><span class="inline-flex items-center justify-center px-2 py-1 text-xl font-bold leading-none text-red-100 bg-red-600 rounded-full">Go</span></a></span><br>
 
                 <p class="ml-6">e-Yantra is a robotics outreach program funded by the Ministry of Education and hosted at IIT Bombay.
                 The goal is to harness the talent of young engineers to to solve problems using technology across a variety of domains such as: agriculture, manufacturing, defense, home, smart-city maintenance and service industries.</p>
