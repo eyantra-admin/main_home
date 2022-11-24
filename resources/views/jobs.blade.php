e21abaf6d8ba26c27b614f95828887835d70c8a4
deepaavudiappan <deepaavudiappan@gmail.com>
2022-11-24 19:45:45 +0530

    changes for new recruitment

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -7,7 +7,7 @@
         <p class="text-xl text-justify">
             e-Yantra is looking to fill the following positions,
         </p>           
-        <p class="text-lg text-red-500 text-justify">If you're interested, please send your resume to <a class="text-blue-600" href="mailto:placements@e-yantra.org">placements@e-yantra.org</a></p>
+        <p class="text-lg text-red-500 text-justify">If you're interested, please apply at the respective link available in the Job Description</p>
     </div>
 
     <!-- computer/IT -->
@@ -17,8 +17,11 @@
         <!-- <p><strong>Positions:</strong> 3</p> -->
         <p>
             <strong>Essential Qualification</strong> 
-            B.E/B.Tech in Computer Science or IT or MCA
+            B.E/B.Tech in Computer Science or IT / MCA  / M.Tech CS/IT
         </p>
+        <p><strong>Positions:</strong> 2</p>
+        <p><strong>Advertisement Reference code:</strong> IRCC/EXT273/2022<br>
+        <a href="https://ep.iitb.ac.in/sap/bc/webdynpro/sap/hrrcf_a_posting_apply?PARAM=cG9zdF9pbnN0X2d1aWQ9MDA1MDU2QUU5RkI4MUVFRDk4QUZGRkE5NjE3RkQ0QTQmY2FuZF90eXBlPUVYVA%3d%3d&sap-theme=sap_corbu&sap-client=300&sap-language=EN#" target="_blank" class="text-blue-500 text-lg font-semibold">Click here to Apply</a> </p>
         <strong>Job Profile:</strong>        
         <ul class="list-disc ml-8">
             <li>Managing application software & web related activities.</li>
@@ -27,12 +30,37 @@
         </ul>    
         <strong>Essential Experience:</strong>
         <ul class="list-disc ml-8">    
-            <li>Experience in Laravel, SQL server, PHP, Ajax, and Java</li>
-            <li>C/C++, Python, based web portals, PostgreSQL/MySQL, Linux platform and Open
-            Source technologies, systems administration & network administration</li>
-            <li>Git, user interfaces, good understanding of web API, experience in systems
-            administration & network administration and in handling web/App/DB servers on
-            Linux platform is desirable.</li>
+            <li>Experience in Django, PHP, Ajax, C/C++, MySQL</li>
+            <li>Experience in version control tools like Git / Bitbucket</li>
+            <li>Additional experience in Laravel, Java, PostgreSQL is appreciated</li>
+            <li>Knowledge of Linux platform and Open Source technologies, systems administration & network administration</li>
+        </ul>    
+    </div>
+
+    <!-- Experienced computer/IT -->
+    <div class="w-full md:w-3/4 mx-auto bg-gradient-to-b from-gray-200 via-gray-100 to-gray-300 rounded-md border-2 border-gray-400 mt-5 p-4">
+        <h3 class="text-xl">Project Software Engineer</h3>
+        <div class="border-b-2 border-gray-400 my-4"></div>
+        <p><strong>Positions:</strong> 2</p>
+        <p>
+            <strong>Essential Qualification</strong> 
+            B.E/B.Tech in Computer Science or IT / MCA  / M.Tech CS/IT
+        </p>
+        <strong>Positions: 2</strong><br>
+        <strong>Job Profile:</strong>        
+        <ul class="list-disc ml-8">
+            <li>Managing application software & web related activities.</li>
+            <li>Databases, architectural design, database design</li>
+            <li>Software Programming/software development</li>
+        </ul>    
+        <strong>Essential Experience:</strong>
+        <ul class="list-disc ml-8">
+            <li>2+ years’ experience in web and software development</li>    
+            <li>Experience in Laravel/Django, PHP, Ajax, C/C++, MySQL</li>
+            <li>Experience in version control tools like Git / Bitbucket</li>
+            <li>Good knowledge of current / emerging technologies and trends</li>
+            <li>Should possess excellent analytical, logical skills and communication skills</li>
+            <li>Knowledge of Linux platform and Open Source technologies, systems administration & network administration</li>
         </ul>    
     </div>
 
@@ -40,7 +68,7 @@
     <div class="w-full md:w-3/4 mx-auto bg-gradient-to-b from-gray-200 via-gray-100 to-gray-300 rounded-md border-2 border-gray-400 mt-5 p-4">
         <h3 class="text-xl">Sr. Project Technical Assistant (Robotics/ Embedded Systems)</h3>
         <div class="border-b-2 border-gray-400 my-4"></div>
-       <!--  <p><strong>Positions:</strong> 6</p> -->
+        <p><strong>Positions:</strong> 10</p>
         <p>
             <strong>Essential Qualification</strong> 
             B.E/B.Tech/ or ME/M.Tech or PG Diploma in Electronics/Electrical/ Instrumentation
@@ -54,16 +82,96 @@
         <ul class="list-disc ml-8">    
             <li>
                 Applicants should have a good background in Embedded systems, hands-on robotics,
-                microcontroller programming, Real-Time Operating Systems. Programming proficiency in C/Embedded-C, Python. 
+                microcontroller programming, Real-Time Operating Systems. Programming proficiency in C/Embedded-C, Python, FPGA, Verilog. 
             </li>
             <li>
                 Applicants must be familiar with Electronic hardware design, Image processing,
                 Robotic Operating System (ROS), Machine Learning etc. Applicants who have
-                performed well in robotics competitions will be given preference. Should possess
-                excellent analytical, logical skills and communication skills.
-            </li>    
+                performed well in robotics competitions will be given preference.</li> 
+            <li>Should possess excellent analytical, logical skills and communication skills.</li>
+                
         </ul>    
     </div>
+
+    <!-- Experienced Design and Product Designer -->
+    <div class="w-full md:w-3/4 mx-auto bg-gradient-to-b from-gray-200 via-gray-100 to-gray-300 rounded-md border-2 border-gray-400 mt-5 p-4">
+        <h3 class="text-xl">Project Research Associate</h3>
+        <div class="border-b-2 border-gray-400 my-4"></div>
+        <p><strong>Positions:</strong> 2</p>
+        <p>
+            <strong>Essential Qualification</strong> 
+            Master of Design / M.Tech in Mechanical
+        </p>
+        <strong>Positions: 2</strong><br>
+        <strong>Job Profile:</strong>        
+        <ul class="list-disc ml-8">
+            <li>Product design and development of robotics system, graphic designing, media handling, content creation</li>
+        </ul>    
+        <strong>Essential Experience:</strong>
+        <ul class="list-disc ml-8">
+            <li>Proficiency in Adobe suite and 3D modeling (CAD)</li>    
+            <li>UX/UI designing and Prototyping techniques</li>
+            <li>Video creation and cinematography</li>
+        </ul>  
+        <strong>Desirable Experience:</strong>
+        <ul class="list-disc ml-8">
+            <li>Adobe Suite, Autodesk, CAD, Woodworking, Mechanical designing, CorelDraw, Google Sketchup, Animation, Video Creation, After effects video editing, cinematography</li>  
+            <li>Should possess excellent analytical and logical skills</li>
+        </ul>  
+    </div>
+
+    <!-- Project Assistant - eLSI -->
+    <div class="w-full md:w-3/4 mx-auto bg-gradient-to-b from-gray-200 via-gray-100 to-gray-300 rounded-md border-2 border-gray-400 mt-5 p-4">
+        <h3 class="text-xl">Project Assistant</h3>
+        <div class="border-b-2 border-gray-400 my-4"></div>
+        <p><strong>Positions:</strong> 2</p>
+        <p>
+            <strong>Essential Qualification</strong> 
+            Bachelor degree in any discipline, Public Relations, Journalism, Communications or a related field 
+        </p>
+        <strong>Positions: 2</strong><br>
+        <strong>Job Profile:</strong>        
+        <ul class="list-disc ml-8">
+            <li>Develop PR campaigns and media relations strategies</li>
+            <li>Write creative content for website</li>
+            <li>Manage promotions and Social Media handles</li>
+        </ul>    
+        <strong>Essential Experience:</strong>
+        <ul class="list-disc ml-8">
+            <li>1+ years’ experience in handling a press release and conference</li>    
+            <li>Excellent written and verbal communication skills</li>
+            <li>Knowledge of consumer marketing and ability to think both creatively and strategically</li>
+            <li>Project and budget management skills</li>
+        </ul>    
+    </div>
+
+    <!-- PR Person -->
+    <div class="w-full md:w-3/4 mx-auto bg-gradient-to-b from-gray-200 via-gray-100 to-gray-300 rounded-md border-2 border-gray-400 mt-5 p-4">
+        <h3 class="text-xl">Project Research Associate</h3>
+        <div class="border-b-2 border-gray-400 my-4"></div>
+        <p><strong>Positions:</strong> 1</p>
+        <p>
+            <strong>Essential Qualification</strong> 
+            Bachelor degree in any discipline / MBA in marketing / Public Relations, Journalism, Communications or a related field  
+        </p>
+        <strong>Positions: 1</strong><br>
+        <strong>Job Profile:</strong>        
+        <ul class="list-disc ml-8">
+            <li>Establish and maintain relationships with community, employee, and public interest groups</li>
+            <li>Develop PR campaigns and media relations strategies</li>
+            <li>Experience in graphics creation and video editing tools</li>
+            <li>Write creative content for website</li>
+            <li>Manage promotions and Social Media handles</li>
+        </ul>    
+        <strong>Essential Experience:</strong>
+        <ul class="list-disc ml-8">
+            <li>2+ years’ experience in handling a press release and conference</li>    
+            <li>Excellent written and verbal communication skills</li>
+            <li>Knowledge of consumer marketing and ability to think both creatively and strategically</li>
+            <li>Project and budget management skills</li>
+        </ul>    
+    </div>
+
 </div>          
 @stop
 
