7a73d771b02c6f07a80461242e163f3c09759598
Premkumar S <kumarsprm@gmail.com>
2024-03-01 18:23:24 +0530

    Alumni Card Added

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -27,6 +27,7 @@
         <div class="container mx-auto px-4">
           <div class="flex flex-wrap justify-center text-center mb-24">
             <div>
+            <h1 class="text-center text-3xl font-semibold pb-3">Core Team</h1>
               <div class="flex flex-wrap">
               <?php
                    $files = glob("img/avatar/core/*.*");
@@ -60,5 +61,43 @@
       </div>
       </section>
     </div>
+    <div class="container-fluid  mx-auto flex flex-col md:flex-row items-center mt-4 ml-6 mr-6 bg-gray-200 md:pt-24 justify-center">
+      <section class="container-fluid  mx-auto flex flex-col md:flex-row items-center ml-6 mr-6 bg-gray-200 md:pt-6 justify-center" data-aos="fade-up" data-aos-duration="1500">
+        <div class="container mx-auto px-4">
+          <div class="flex flex-wrap justify-center text-center mb-24">
+            <div>
+            <h1 class="text-center text-3xl font-semibold pb-3">Alumni</h1>
+              <div class="flex flex-wrap">
+              <?php
+                   $files = glob("img/avatar/alumni/*.*");
+                  for ($i=0; $i<count($files); $i++)
+                  {
+                      $path = $files[$i];
+                      $pathpieces = explode("/", $path);
 
+                      $filename = $pathpieces[3];
+
+                      $member = explode("~", substr("$filename", 2, -4));
+
+                      $displayname = str_replace("_"," ","$member[0]");
+                      $displaytitle = str_replace("_"," ","$member[1]");
+
+                      // echo '<div class="flex flex-wrap">';
+                      echo '<div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">';
+                      echo '<div class="px-6 pt-6">';
+                      echo '<img src="',$path,'" alt="',$displayname,'" class="shadow-lg rounded-full max-w-full mx-auto"/>';
+                      echo '<div class="pt-6 text-center">';
+                      echo '<h5 class="text-xl font-bold text-gray-700">',$displayname,'</h5>';
+                      echo '<p class="mt-1 text-sm text-gray-500 font-semibold">',$displaytitle,'</P>';
+                      echo '</div>';
+                      echo '</div>';
+                      echo '</div>';
+                  }
+                ?>
+              </div>
+          </div>
+        </div>
+      </div>
+      </section>
+    </div>
 @stop
