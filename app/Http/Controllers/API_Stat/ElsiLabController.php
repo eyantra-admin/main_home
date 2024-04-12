c65580de1f292fcc0dbe6a221a83c52d1dbaa0b8
sachinuthale <sachinuthale@gmail.com>
2024-04-12 14:47:22 +0530

    api: elsi lab count

---
+++ app/Http/Controllers/API_Stat/ElsiLabController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\API_Stat;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use DB;
+use DateTime;
+
+class ElsiLabController extends Controller
+{
+    //
+    public function elsiLabCount(Request $request, $year = null){
+    	$current_year = (new DateTime)->format("Y");
+    	//if year is nu
+    	if($year == null || $year >= $current_year){
+    		$year = $current_year - 1;
+    	}
+
+    	if($year <= 2013){
+    		$year = 2013;
+    	}
+
+		$data = DB::table('api_elsi_lab_count_yearwise')
+			->where(['year' => $year, 'active' => 1])
+			->where('number_of_labs','>',0)
+			->orderBy('state', 'ASC')
+			->get(['state', 'year', 'number_of_labs']);
+
+    	return response()->json([
+    		'api_title' => 'Number of eLSI Labs Established',
+    		'frequency' => 'Yearly',
+    		'description' => 'https://e-yantra.org/elsi', 
+    		'data' => $data,
+    	]);
+    }
+}
